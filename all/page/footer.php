6e530e351474d74e1ddf9bdd98677d557823846e
AnThanh19 <92195327+AnThanh19@users.noreply.github.com>
2021-12-01 17:50:51 +0700

    Add files via upload

---
+++ all/page/footer.php
@@ -0,0 +1,64 @@
+<footer class="footer">
+            <div class="grid wide">
+                <div class="row footer-row">
+                    <div class="col l-3 m-4 c-6">
+                        <h3 class="footer-title">CineSV</h3>
+                        <ul class="footer-list">
+                            <a href="" class="footer-link">Giới thiệu</a>
+                            <a href="" class="footer-link">Tiện ích Online</a>
+                            <a href="" class="footer-link">Thẻ quà tặng</a>
+                            <a href="" class="footer-link">Tuyển dụng</a>
+                            <a href="" class="footer-link">Liên hệ quảng cáo CGV</a>
+                        </ul>
+                    </div>
+
+                    <div class="col l-3 m-4 c-6">
+                        <h3 class="footer-title">Điều khoản sử dụng</h3>
+                        <ul class="footer-list">
+                            <a href="" class="footer-link">Điều khoản chung</a>
+                            <a href="" class="footer-link">Điều khoản giao dịch</a>
+                            <a href="" class="footer-link">Chính sách thanh toán</a>
+                            <a href="" class="footer-link">Chính sách bảo mật</a>
+                            <a href="" class="footer-link">Câu hỏi thường gặp</a>
+                        </ul>
+                    </div>
+
+                    <div class="col l-3 m-4 c-6 footer-socials">
+                        <h3 class="footer-title">CineSV</h3>
+                        <a href="https://www.facebook.com/Tins.Grace.vl/" target="_blank" class="footer-link-socials">
+                            <i class="footer-icon-socials fab fa-facebook-square" style="color: rgb(12, 55, 150); padding-left: 0;"></i>
+                        </a>
+                        <a href="https://www.instagram.com/lethanhtin____/" target="_blank" class="footer-link-socials">
+                            <i class="footer-icon-socials fab fa-instagram-square"  style="color: rgb(219, 58, 152);"></i>
+                        </a>
+                        <a href="https://www.youtube.com/cgvvietnam" target="_blank" class="footer-link-socials">
+                            <i class="footer-icon-socials fab fa-youtube-square" style="color: rgb(161, 31, 31);"></i>
+                        </a>
+                        <a href="https://twitter.com/CGV_ID" target="_blank" class="footer-link-socials">
+                            <i class="footer-icon-socials fab fa-twitter-square" style="color: rgb(42, 146, 187);"></i>
+                        </a>
+                        <a href="http://online.gov.vn/Home/WebDetails/30270" target="_blank" class="bo-cong-thuong"></a>
+                    </div>
+
+                    <div class="col l-3 m-4 c-6 hide-on-tablet">
+                        <h3 class="footer-title">Chăm sóc khách hàng</h3>
+                        <ul class="footer-list">
+                            <h4 href="" class="footer-link">Hotline: 0978827442</h4>
+                            <h4 href="" class="footer-link">Giờ làm việc: 8:00 - 22:00 (Tất cả các ngày bao gồm cả Lễ Tết)</h4>
+                            <h4 href="" class="footer-link">Email hỗ trợ: thanlonglua97@gmail.com</h4>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="footer__about">
+                    <div class="footer-logo" style="background-image: url(/assets/img/Slider/common_sprite_area.png);"></div>
+                    <div class="footer__contact">
+                        <h3 class="footer-name">CÔNG TY TNHH MTV CineSV</h3>
+                        <h4 class="footer-sub">Giấy CNĐKDN: 0303675393, đăng ký lần đầu ngày 31/7/2008, đăng ký thay đổi lần thứ 5 ngày 14/10/2015, cấp bởi Sở KHĐT thành phố Hồ Chí Minh.</h4>
+                        <h4 class="footer-sub">Địa Chỉ: Khu phố 6, Phường Linh Trung, Thành Phố Thủ Đức, TPHCM.</h4>
+                        <h4 class="footer-sub">COPYRIGHT 2021 CJ CGV. All RIGHTS RESERVED</h4>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
---
+++ all/page/header.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="./assets/img/title-icon.png">
+    <title>CGV Cinemas VietNam</title>
+    <link rel="stylesheet" href="https://pagecdn.io/lib/normalize/8.0.1/normalize.min.css">
+    <link rel="stylesheet" href="../../fontawesome-free-5.15.4-web/css/all.min.css">
+    <link rel="stylesheet" href="../../css/base.css">
+    <link rel="stylesheet" href="../../css/main.css">
+    <link rel="stylesheet" href="../../css/grid.css">
+    <link rel="stylesheet" href="../../css/responsive.css">
+    
+</head>
+<body>
+    <div class="app">
+    <header class="header">
+            <a href="../../../index.html" class="header__logo hide-on-tablet-mobile">
+                <img src="../../img/logo-sv" alt="" class="header__logo-img">
+            </a>
+            
+            <label for="tablet-mobile-bars-checkbox" class="header__bars">
+                <i class="header__bars-icon fas fa-bars"></i>
+            </label>
+            <input type="checkbox" hidden id="tablet-mobile-bars-checkbox" class="header__navbar-bars-checkbox">
+            <label for="tablet-mobile-bars-checkbox" class="nav__overlay"></label>
+            <nav class="navbar">
+                <ul class="navbar-list">
+                    <li class="navbar-item noHover">
+                        <label for="subnav-film-checkbox" class="navbar-link">Phim</label>
+                        <input type="checkbox" hidden id="subnav-film-checkbox" class="suvnav-checkbox">                      
+                        
+                            <ul class="subnav-list">
+                                <li class="subnav-item">
+                                    <a href="../../pages/phimDangChieu.html" class="subnav-link">Phim đang chiếu</a>
+                                </li>
+                                <li class="subnav-item">
+                                    <a href="../../pages/Phim/phimSapChieu.html" class="subnav-link">Phim sắp chiếu</a>
+                                </li>
+                            </ul>
+                        
+                    </li>
+                    <li class="navbar-item noHover">
+                        <a href="../../pages/Rap/allRap.php" class="navbar-link">Rạp phim</a>
+                    </li>
+                    <li class="navbar-item noHover">
+                        <a href="../../pages/tintuc/tinTuc.php" class="navbar-link">Tin tức</a>
+                        
+                    </li>
+                    <li class="navbar-item noHover">
+                        <label for="subnav-cultural-checkbox" class="navbar-link">Cultureplex</label>
+                            <input type="checkbox" hidden id="subnav-cultural-checkbox" class="suvnav-checkbox">
+    
+                        
+                            <ul class="subnav-list">
+                                <li class="subnav-item">
+                                    <a href="" class="subnav-link">Sự kiện</a>
+                                </li>
+                                <li class="subnav-item">
+                                    <a href="" class="subnav-link">Thẻ quà tặng</a>
+                                </li>
+                            </ul>
+                        
+                    </li>
+                </ul>
+                
+            </nav>
+            <div class="header__user">
+                <div class="header__user-info">
+                    <i class="header__user-icon fas fa-user-circle"></i>
+                    <span class="header__user-name">Le Thanh Tin</span>
+                </div>
+                <div class="header__user-options">
+                    <ul class="user-options-list">
+                        <li class="user-options-item">
+                            <a href="" class="user-option-link">Tài khoản</a>
+                        </li>
+                        <li class="user-options-item">
+                            <a href="" class="user-option-link">Cài đặt</a>
+                        </li>
+                        <li class="user-options-item">
+                            <a href="" class="user-option-link">Đăng xuất</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="login">
+                <ul class="login-list">
+                    <li class="login-item login-item-sign-in">
+                        <label href="" class="login-link js-login-form">Đăng nhập</label>
+                    </li>
+                    <li class="login-item login-item-register">
+                        <label href="" class="login-link js-register-form">Đăng ký</label>
+                    </li>
+                </ul>
+            </div>
+    
+        </header>
+        
